04502d21d549d96f4d654a9a6d4afa85715be34f
Walfenstaine <121248230+Walfenstaine@users.noreply.github.com>
2024-09-10 10:06:37 +0300

    111212

--- Assets/Proect/Skripts/Coin.cs
+++ Assets/Proect/Skripts/Coin.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    public AudioClip clip;
-    public Data data;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            data.record += 1;
-            SoundPlayer.regit.Play(clip,1);
+            Lives.rid.OnCoin();
             Destroy(gameObject);
         }
     }
--- Assets/Proect/Skripts/Lives.cs
+++ Assets/Proect/Skripts/Lives.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class Lives : MonoBehaviour
 {
+    public AudioClip clip, liv;
+    public static Lives rid { get; set; }
+    private int timer = 100;
+    void Awake()
+    {
+        if (rid == null)
+        {
+            rid = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    void OnDestroy()
+    {
+        rid = null;
+    }
     public Data data;
     public GameObject[] herses;
     void Start()
@@ -19,5 +37,21 @@ public class Lives : MonoBehaviour
                 herses[i].SetActive(false);
             }
         }
+    }
+    public void OnCoin() 
+    {
+        if (timer > 0)
+        {
+            SoundPlayer.regit.Play(clip,1);
+            timer -= 1;
+        }
+        else 
+        {
+            SoundPlayer.regit.Play(liv, 1);
+            data.lives += 1;
+            timer = 100;
+            Start();
+        }
+        data.coins += 1;
     }
 }
